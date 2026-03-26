8a7ea3b473bb0344122cc5c964b16c2483d1593f
cmd-hue <guh.huh.egg4@gmail.com>
2026-03-26 18:35:41 -0400

    Add files via upload

---
+++ live.js
@@ -0,0 +1,38 @@
+var _____WB$wombat$assign$function_____=function(name){return (self._wb_wombat && self._wb_wombat.local_init && self._wb_wombat.local_init(name))||self[name];};if(!self.__WB_pmw){self.__WB_pmw=function(obj){this.__WB_source=obj;return this;}}{
+let window = _____WB$wombat$assign$function_____("window");
+let self = _____WB$wombat$assign$function_____("self");
+let document = _____WB$wombat$assign$function_____("document");
+let location = _____WB$wombat$assign$function_____("location");
+let top = _____WB$wombat$assign$function_____("top");
+let parent = _____WB$wombat$assign$function_____("parent");
+let frames = _____WB$wombat$assign$function_____("frames");
+let opens = _____WB$wombat$assign$function_____("opens");
+window.labels={'live':'6d2027f9'};(function(){var a=window.labels;window.jstiming&&window.jstiming.load&&window.jstiming.load.tick("ld_s");var b=window.devjs,c=/[?&]debugjs=1/.exec(window.location.href),f=/[?&]localPlayer=1/.exec(window.location.href),g=/[?&]mediaDiagnostics=1/.exec(window.location.href),h=window.local_label,k=/[?&]reversePairingCode=/.exec(window.location.href),l=/[?&]launch=preload/.exec(window.location.href),n=/[?&]v=[\w\+\/\-_=]+/.exec(window.location.href);window.label=a?h in a?a[h]:/^[a-f0-9]{8}$/.test(h)?h:a.live:"unknown";
+var p=window.appRoot+window.label;function q(d){document.write('<script src="'+d+'">\x3c/script>')}function r(d){document.write("<script>"+d+"\x3c/script>")}function s(d){var e=document.createElement("link");e.setAttribute("rel","stylesheet");e.setAttribute("type","text/css");e.setAttribute("href",d);d=document.getElementsByTagName("head")[0];d.insertBefore(e,d.lastChild)}
+window.removeAngularCss=function(){if(angular&&angular.element)for(var d=angular.element(document).find("style"),e,t=d.length,m=0;m<t;++m)e=d[m],0<=e.innerText.indexOf("ng-cloak")&&angular.element(e).remove()};window.initializeOrRedirect=function(d){window.jstiming.load.tick("js_r");yt&&yt.tv&&yt.tv.initializer?yt.tv.initializer(d):window.location="https://web.archive.org/web/20131231162335/http://www.youtube.com/error?src=404"};
+b?(window.CLOSURE_BASE_PATH="/javascript/closure/",q(p+"/angular.js"),q(p+"/lasagna-parse.js"),q(CLOSURE_BASE_PATH+"base.js"),q("/i18n/input/javascript/deps.js"),q("/video/youtube/src/web/javascript/deps-runfiles.js"),q(p+"/deps.js"),q(p+"/js/initializer.js"),q(p+"/js/preload_templates.js"),r("yt.tv.preloadTemplates.appPath = '"+p+"';"),q(p+"/html-list.js"),s(p+"/css/main.css"),s(p+"/css/search.css"),s(p+"/css/browse.css"),s(p+"/css/watch.css"),s(p+"/css/pair.css"),s(p+"/css/toasts.css"),s(p+"/css/dialog.css"),
+s(p+"/css/lang.css"),s(p+"/css/voice.css"),s(p+"/css/devices.css"),s(p+"/css/media_queries.css"),s(p+"/css/experiments.css"),s(p+"/css/call_to_cast.css")):c?(window.CLOSURE_NO_DEPS=!0,s(p+"/app-prod.css"),q(p+"/app-concat-bundle.js")):(s(p+"/app-prod.css"),q(p+"/app-prod.js"),(k||l||n)&&q(window.environment.player_url));window.checkBrokenLabel=function(){"undefined"==typeof yt&&h&&(window.location.href=window.location.href.replace(/([?&])label=[^&]+&?/,"$1stick=0&"))};r("checkBrokenLabel()");r("removeAngularCss()");
+f&&(window.environment.player_url=c||b?"/video/youtube/src/web/javascript/debug-tv-player.js":"/video/youtube/src/web/javascript/tv-player.js");g&&(c||b?q(p+"/modules/media-diagnostics-debug.js"):q(p+"/modules/media-diagnostics.js"));b||r("initializeOrRedirect('"+p+"');");})();
+
+}
+
+/*
+     FILE ARCHIVED ON 16:23:35 Dec 31, 2013 AND RETRIEVED FROM THE
+     INTERNET ARCHIVE ON 22:32:08 Mar 26, 2026.
+     JAVASCRIPT APPENDED BY WAYBACK MACHINE, COPYRIGHT INTERNET ARCHIVE.
+
+     ALL OTHER CONTENT MAY ALSO BE PROTECTED BY COPYRIGHT (17 U.S.C.
+     SECTION 108(a)(3)).
+*/
+/*
+playback timings (ms):
+  captures_list: 0.466
+  exclusion.robots: 0.015
+  exclusion.robots.policy: 0.007
+  esindex: 0.009
+  cdx.remote: 24.45
+  LoadShardBlock: 61.443 (3)
+  PetaboxLoader3.datanode: 90.138 (4)
+  load_resource: 119.122
+  PetaboxLoader3.resolve: 74.363
+*/
